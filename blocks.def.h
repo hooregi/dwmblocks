8e6d803d2a8ccd99ad16fcc0272620838b974dce
Hooregi <azerpa586@gmail.com>
2023-08-14 05:40:47 +0200

    modified volume timer

--- blocks.def.h
+++ blocks.def.h
@@ -2,11 +2,9 @@
 static const Block blocks[] = {
 // icon, command, update interval, update signal
 	{"", "cat /tmp/recordingicon 2>/dev/null", 0, 9},
-//{"", "~/.local/bin/statusbar/sb-media", 0, 11},
 	{"", "~/.local/bin/statusbar/sb-cpu", 10, 18},
-//{"", "~/.local/bin/statusbar/sb-mailbox", 180, 12},
   {"", "~/.local/bin/statusbar/sb-wifi", 5, 4},
-	{"", "~/.local/bin/statusbar/sb-volume", 0, 10},
+	{"", "~/.local/bin/statusbar/sb-volume", 10, 10},
 	{"", "~/.local/bin/statusbar/sb-battery", 5, 3},
 	{"", "~/.local/bin/statusbar/sb-time", 60, 1},
 	{"", "~/.local/bin/statusbar/sb-date", 60, 1},
